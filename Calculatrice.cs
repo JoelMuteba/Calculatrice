b5c7e102c0a30c6a65f0b7b896f6f83a1b4d286a
JoelMuteba <mutebakandejoel@hotmail.fr>
2023-04-11 19:19:54 -0400

    implementation soustraction

--- Calculatrice.cs
+++ Calculatrice.cs
@@ -6,5 +6,10 @@
         {
             Console.WriteLine("Hello, World!!!");
         }
+
+        public static int Soustraction(int a, int b)
+        {
+            return a - b;
+        }
     }
 }

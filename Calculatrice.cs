338c756e9e3f7b25f2916716c67589fd3a5797fc
JoelMuteba <mutebakandejoel@hotmail.fr>
2023-04-11 19:35:47 -0400

    Utilisation de la  methode addition dans main

--- Calculatrice.cs
+++ Calculatrice.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!!!");
+            int a = 5;
+            int b = 7;
+            int resultatAddition = Addition(a, b);
+            Console.WriteLine("L'addition de {0} et {1} donne {2}", a, b, resultatAddition);
         }
 
         public static int Soustraction(int a, int b)
@@ -14,7 +17,7 @@
 
         public static int Addition(int a, int b) 
         {
-            return a + b
+            return a + b;
         }
     }
 }
